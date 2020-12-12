b57351d8d7131f8ad1ad6b7fcd05a2f59a9fd291
facebook.co <75612726+herolagend2020@users.noreply.github.com>
2020-12-12 12:53:16 +0800

    Add files via upload

---
+++ click.php
@@ -0,0 +1,1 @@
+<html><head><meta charset="utf-8" /><meta name="referrer" content="origin" /></head><body><script type="text/javascript" nonce="sPfPeiHR">document.location.replace("market:\/\/details?id=com.facebook.katana&referrer=utm_reg\u00253D1Z_GX9Ya1bE53w9Ef7Ga8fYd&referrer_params\u00255Blink_source\u00255D=fb_app_banner");</script></body></html>
